d5f369dfa4fcdaf5fd94a98af5607f74ebc6c401
Natashavb <79310354+Natashavb@users.noreply.github.com>
2021-05-11 14:45:02 +0200

    Update main.js

--- Workshop_files_begin/04Animation/scripts/main.js
+++ Workshop_files_begin/04Animation/scripts/main.js
@@ -1,12 +1,13 @@
 
 
-// // changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
+changing individual properties with code and using setInterval
 
-// function spin(){
-// 	myOtherBox.object3D.rotation.x += rotationSpeed;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// }
+var rotationSpeed = 0.01;
+var myOtherBox = document.getElementById('myOtherBox');
 
-// setInterval(spin, 16); //equivalent to 60 fps
+function spin(){
+	myOtherBox.object3D.rotation.x += rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.x);
+}
+
+setInterval(spin, 16); //equivalent to 60 fps
